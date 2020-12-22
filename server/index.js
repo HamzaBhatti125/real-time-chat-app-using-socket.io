209073ac21d032da4ff5ee032d5eb04cac0a9ac1
Hamza Bhatti <hbhati006@gmail.com>
2020-12-22 21:18:52 +0500

    commit till infoBar

--- server/index.js
+++ server/index.js
@@ -2,16 +2,48 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUSer, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
+
+    socket.on('join', ({ name, room }, callback) => {
+        //console.log(name, room);
+
+        const { error, user } = addUSer({ id: socket.id, name, room });
+
+        if (error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` })
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!`});
+
+        socket.join(user.room);
+
+        callback();
+
+    })
+
+    socket.on('sendMessage', (message, callback)=>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
+
+    })
 
     socket.on('disconnect', () => {
         console.log('User had left!!!');
